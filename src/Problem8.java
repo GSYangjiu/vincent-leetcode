eca9c96f71bdc7639d2817cfa97127aff1144b87
vincent <1094259423@qq.com>
2022-02-08 09:55:48 +0800

    refactor：Problem8 字符串转换整数

--- src/Problem8.java
+++ src/Problem8.java
@@ -8,7 +8,7 @@ import java.util.Map;
  * Description：字符串转换整数 (atoi) https://leetcode-cn.com/problems/string-to-integer-atoi/
  */
 public class Problem8 {
-    public static int myAtoi(String str) {
+    public int myAtoi(String str) {
         Automaton automaton = new Automaton();
         int length = str.length();
         for (int i = 0; i < length; ++i) {
@@ -17,43 +17,44 @@ public class Problem8 {
         return (int) (automaton.sign * automaton.ans);
     }
 
+    //状态机
+    class Automaton {
+        public int sign = 1;
+        public long ans = 0;
+        private String state = "start";
+        private Map<String, String[]> table = new HashMap<String, String[]>() {{
+            put("start", new String[]{"start", "signed", "in_number", "end"});
+            put("signed", new String[]{"end", "end", "in_number", "end"});
+            put("in_number", new String[]{"end", "end", "in_number", "end"});
+            put("end", new String[]{"end", "end", "end", "end"});
+        }};
+
+        public void get(char c) {
+            state = table.get(state)[get_col(c)];
+            if ("in_number".equals(state)) {
+                ans = ans * 10 + c - '0';
+                ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+            } else if ("signed".equals(state)) {
+                sign = c == '+' ? 1 : -1;
+            }
+        }
+
+        private int get_col(char c) {
+            if (c == ' ') {
+                return 0;
+            }
+            if (c == '+' || c == '-') {
+                return 1;
+            }
+            if (Character.isDigit(c)) {
+                return 2;
+            }
+            return 3;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(myAtoi("  -91+19 aw "));
-    }
-}
-
-//状态机
-class Automaton {
-    public int sign = 1;
-    public long ans = 0;
-    private String state = "start";
-    private Map<String, String[]> table = new HashMap<String, String[]>() {{
-        put("start", new String[]{"start", "signed", "in_number", "end"});
-        put("signed", new String[]{"end", "end", "in_number", "end"});
-        put("in_number", new String[]{"end", "end", "in_number", "end"});
-        put("end", new String[]{"end", "end", "end", "end"});
-    }};
-
-    public void get(char c) {
-        state = table.get(state)[get_col(c)];
-        if ("in_number".equals(state)) {
-            ans = ans * 10 + c - '0';
-            ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
-        } else if ("signed".equals(state)) {
-            sign = c == '+' ? 1 : -1;
-        }
-    }
-
-    private int get_col(char c) {
-        if (c == ' ') {
-            return 0;
-        }
-        if (c == '+' || c == '-') {
-            return 1;
-        }
-        if (Character.isDigit(c)) {
-            return 2;
-        }
-        return 3;
+        Problem8 problem8 = new Problem8();
+        System.out.println(problem8.myAtoi("  -91+19 aw "));
     }
 }
