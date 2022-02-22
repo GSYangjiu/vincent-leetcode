dd871bd781e7108e1307515939c36d53f9283dc4
vincent <1094259423@qq.com>
2022-02-22 23:48:50 +0800

    feat：Problem31 下一个排列

---
+++ src/Problem31.java
@@ -0,0 +1,39 @@
+/**
+ * @author Vincent
+ * @date 2022/2/22 10:55 下午
+ * Email: vincent1094259423@yahoo.com
+ * Title: 下一个排列
+ * URL: https://leetcode-cn.com/problems/next-permutation/
+ * Description：
+ */
+public class Problem31 {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
