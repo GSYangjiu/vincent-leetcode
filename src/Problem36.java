51239599ed0466055e16f8c287f06da9da6da3b6
vincent <1094259423@qq.com>
2022-03-01 12:45:36 +0800

    feat：Problem36 有效的数独

---
+++ src/Problem36.java
@@ -0,0 +1,30 @@
+/**
+ * @author Vincent
+ * @date 2022/3/1 10:06 上午
+ * Email: vincent1094259423@yahoo.com
+ * Title: 有效的数独
+ * URL: https://leetcode-cn.com/problems/valid-sudoku/
+ * Description：
+ */
+public class Problem36 {
+    public boolean isValidSudoku(char[][] board) {
+        int[][] rows = new int[9][9];
+        int[][] columns = new int[9][9];
+        int[][][] subboxes = new int[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int index = c - '0' - 1;
+                    rows[i][index]++;
+                    columns[j][index]++;
+                    subboxes[i / 3][j / 3][index]++;
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i / 3][j / 3][index] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
