de3a86e02a1eefd12b84256ff2355fd69c977eef
vincent <1094259423@qq.com>
2022-03-08 18:35:36 +0800

    feat：Problem278 第一个错误的版本

---
+++ src/Problem278.java
@@ -0,0 +1,24 @@
+/**
+ * @author Vincent
+ * @date 2022/3/8 5:56 下午
+ * Email: vincent1094259423@yahoo.com
+ * Title: 第一个错误的版本
+ * URL: https://leetcode-cn.com/problems/first-bad-version/
+ * Description：
+ */
+public class Problem278 {
+//    public class Solution extends VersionControl {
+//        public int firstBadVersion(int n) {
+//            int l = 1, r = n;
+//            while (l < r) {
+//                int mid = l + (r - l) / 2;
+//                if (isBadVersion(mid)) {
+//                    r = mid;
+//                } else {
+//                    l = mid + 1;
+//                }
+//            }
+//            return l;
+//        }
+//    }
+}
