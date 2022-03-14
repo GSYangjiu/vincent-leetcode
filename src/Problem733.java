5d73e36ec61819576809fa0d1a1665fbc7ac81b8
vincent <1094259423@qq.com>
2022-03-14 15:39:07 +0800

    refactor：Problem733 图像渲染

--- src/Problem733.java
+++ src/Problem733.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -64,6 +66,36 @@ public class Problem733 {
                 cache[x][y - 1] = 1;
                 stack.push(new Node(x, y - 1));
             }
+        }
+    }
+
+    //代码优化
+    class Solution1 {
+        int[] dx = {1, 0, 0, -1};
+        int[] dy = {0, 1, -1, 0};
+
+        public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+            int currColor = image[sr][sc];
+            if (currColor == newColor) {
+                return image;
+            }
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(new int[]{sr, sc});
+            image[sr][sc] = newColor;
+            while (!queue.isEmpty()) {
+                int[] node = queue.poll();
+                int x = node[0];
+                int y = node[1];
+
+                for (int i = 0; i < 4; i++) {
+                    int mx = x + dx[i], my = y + dy[i];
+                    if (mx >= 0 && mx < image.length && my >= 0 && my < image[0].length && image[mx][my] == currColor) {
+                        queue.offer(new int[]{mx, my});
+                        image[mx][my] = newColor;
+                    }
+                }
+            }
+            return image;
         }
     }
 }
