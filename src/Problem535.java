c8dec580f1c1845c888f7df7d4d44a7b5c2de87c
vincent <1094259423@qq.com>
2022-06-29 14:58:18 +0800

    feat：Problem535 TinyURL的加密与解密

---
+++ src/Problem535.java
@@ -0,0 +1,23 @@
+/**
+ * @author Vincent
+ * @date 2022/6/29 2:24 下午
+ * Email: vincent1094259423@yahoo.com
+ * Title: TinyURL 的加密与解密
+ * URL: https://leetcode.cn/problems/encode-and-decode-tinyurl/
+ * Description：
+ */
+public class Problem535 {
+    public class Codec {
+
+
+        // Encodes a URL to a shortened URL.
+        public String encode(String longUrl) {
+            return longUrl;
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public String decode(String shortUrl) {
+            return shortUrl;
+        }
+    }
+}
